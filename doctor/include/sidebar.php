bedbbb5201c2fe31f686de3632b649d8dd3a5477
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2023-05-18 18:39:24 +0500

    Add files via upload

---
+++ doctor/include/sidebar.php
@@ -0,0 +1,24 @@
+<!-- sidebar menu -->
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+	<div class="menu_section">
+		<ul class="nav side-menu">
+			<li><a href="dashboard.php"><i class="fa fa-home"></i> Dashboard</a></li>
+			<li><a href="appointment-history.php"><i class="fa fa-line-chart"></i> Appointments</a></li>
+			<li><a href="update-avail.php"><i class="fa fa-square"></i> Update Availability</a></li>
+			<li><a href="edit-profile.php"><i class="fa fa-smile-o"></i> Modify Profile</a></li>
+			<li><a href="view-doctordetails.php"><i class="fa fa-sign-out"></i> View Profile</a></li>
+			<li><a href="add-detail.php"><i class="fa fa-plus"></i> Add Detail To Profile</a></li>
+			<li><a href="view-disease.php"><i class="fa fa-eye"></i> View Disease Information</a></li>
+			<li><a href="change-password.php"><i class="fa fa-key"></i> Change Password</a></li>
+			<li><a href="logout.php"><i class="fa fa-sign-out"></i> Log Out</a></li>
+		
+
+		</ul>
+	</div>
+	
+</div>
+
+
+
+
+
