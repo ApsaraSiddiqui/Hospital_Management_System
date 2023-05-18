44f13725e01b897285ad9014e79e564835fb85d1
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2023-05-18 18:36:51 +0500

    Add files via upload

---
+++ admin/include/sidebar.php
@@ -0,0 +1,50 @@
+<!-- sidebar menu -->
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+	<div class="menu_section">
+		
+		<ul class="nav side-menu">
+			<li><a href="dashboard.php"><i class="fa fa-home"></i> Dashboard</a></li>
+			<li><a><i class="fa fa-user-md"></i> Doctors <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					
+					<li><a href="add-doctor.php">Add Doctor</a></li>
+					<li><a href="manage-doctors.php">Manage Doctors</a></li>
+					
+				</ul>
+			</li>
+			<li><a><i class="fa fa-users"></i> Users <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="manage-users.php">Manage Users</a></li>
+					<li><a href="user-logs.php">User Session Logs</a></li>
+				</ul>
+			</li>
+
+			<li><a><i class="fa fa-user"></i> Patients <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="manage-patient.php">Manage Patients</a></li>
+					<li><a href="add-patients.php">Add Patients</a></li>
+				</ul>
+			</li>
+
+			<li><a><i class="fa fa-home"></i> Cities <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="manage-cities.php">Manage Cities</a></li>
+					<li><a href="add-cities.php">Add Cities</a></li>
+				</ul>
+			</li>
+
+			<li><a><i class="fa fa-info"></i> Information <span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="manage-info.php">Manage Info</a></li>
+					<li><a href="add-info.php">Add Info</a></li>
+				</ul>
+			</li>
+			
+			<li><a href="change-password.php"><i class="fa fa-key"></i> Change Password</a></li>
+			<li><a href="logout.php"><i class="fa fa-sign-out"></i> Log Out</a></li>
+
+
+		</ul>
+	</div>
+	
+</div>
